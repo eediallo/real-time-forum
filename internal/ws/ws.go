ca59d10e09e14f3760f46b84ef2d0c3511acc0ad
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-28 19:42:47 +0000

    adds error handling

--- internal/ws/ws.go
+++ internal/ws/ws.go
@@ -3,15 +3,20 @@ package ws
 import (
 	"log"
 	"net/http"
+	"sort"
+	"sync"
 
 	"fmt"
 
+	"github.com/eediallo/real_time_forum/internal/db"
 	"github.com/gorilla/websocket"
 )
 
-var wsChan = make(chan WsPayload)
-
-var clients = make(map[WebSocketConnection]string)
+var (
+	wsChan  = make(chan WsPayload)
+	clients = make(map[WebSocketConnection]string)
+	mu      sync.Mutex
+)
 
 var upgradeConnection = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -23,11 +28,11 @@ type WebSocketConnection struct {
 	*websocket.Conn
 }
 
-// WsJonResponse define the response sent back from the websocket
 type WsJonResponse struct {
-	Action      string `json:"action"`
-	Message     string `json:"message"`
-	MessageType string `json:"message_type"`
+	Action         string   `json:"action"`
+	Message        string   `json:"message"`
+	MessageType    string   `json:"message_type"`
+	ConnectedUsers []string `json:"connected_users"`
 }
 
 type WsPayload struct {
@@ -35,67 +40,108 @@ type WsPayload struct {
 	Username string              `json:"username"`
 	Message  string              `json:"message"`
 	Conn     WebSocketConnection `json:"-"`
+	User     db.User
 }
 
-// WsEndpoint upgrade connection to websocket
 func WsEndpoint(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgradeConnection.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println(err)
+		log.Println("Upgrade error:", err)
+		return
 	}
 
 	log.Println("Client connected to endpoint")
 
-	var response WsJonResponse
-	response.Message = `<em><small>Connected to server</small><em>`
-
 	conn := WebSocketConnection{Conn: ws}
+	mu.Lock()
 	clients[conn] = ""
+	mu.Unlock()
 
+	response := WsJonResponse{Message: `<em><small>Connected to server</small><em>`}
 	err = ws.WriteJSON(response)
 	if err != nil {
-		log.Println(err)
+		log.Println("WriteJSON error:", err)
+		return
 	}
 
-	go ListenForWs((&conn))
+	go ListenForWs(&conn)
 }
 
 func ListenForWs(conn *WebSocketConnection) {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Println("Error", fmt.Sprintf("%v", r))
+			log.Println("Error:", fmt.Sprintf("%v", r))
 		}
+		mu.Lock()
+		delete(clients, *conn)
+		mu.Unlock()
+		conn.Close()
 	}()
 
-	var payload WsPayload
 	for {
+		var payload WsPayload
 		err := conn.ReadJSON(&payload)
 		if err != nil {
-			// do nothing
-		} else {
-			payload.Conn = *conn
-			wsChan <- payload
+			log.Printf("ReadJSON error for client %v: %v", conn, err)
+			return
 		}
+		payload.Conn = *conn
+		wsChan <- payload
 	}
 }
 
 func ListenToWsChannel() {
-	var response WsJonResponse
-
 	for {
 		e := <-wsChan
+		var response WsJonResponse
 
-		response.Action = "Got here"
-		response.Message = fmt.Sprintf("Some message, and action was %s", e.Action)
-		BroadCastToAll(response)
+		switch e.Action {
+		case "username":
+			mu.Lock()
+			clients[e.Conn] = e.User.Username
+			users := getUserList()
+			mu.Unlock()
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			BroadCastToAll(response)
+
+		case "left":
+			mu.Lock()
+			delete(clients, e.Conn)
+			users := getUserList()
+			mu.Unlock()
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			BroadCastToAll(response)
+
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", e.User.Username, e.Message)
+			BroadCastToAll(response)
+		}
 	}
 }
 
+func getUserList() []string {
+	mu.Lock()
+	defer mu.Unlock()
+	var userList []string
+	for _, username := range clients {
+		if username != "" {
+			userList = append(userList, username)
+		}
+	}
+	sort.Strings(userList)
+	return userList
+}
+
 func BroadCastToAll(response WsJonResponse) {
+	mu.Lock()
+	defer mu.Unlock()
 	for client := range clients {
 		err := client.WriteJSON(response)
 		if err != nil {
-			log.Println("Websocket err")
+			log.Printf("WebSocket error for client %v: %v", client, err)
 			_ = client.Close()
 			delete(clients, client)
 		}
