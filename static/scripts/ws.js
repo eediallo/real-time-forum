616ad9eb2ae5ade2444088cd1f421646522555fc
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-28 14:57:43 +0000

    get online users

--- static/scripts/ws.js
+++ static/scripts/ws.js
@@ -5,4 +5,29 @@ document.addEventListener("DOMContentLoaded", () => {
   socket.onopen = () => {
     console.log("Successully connected");
   };
+
+  socket.onclose = () => {
+    console.log("Connection closed");
+  };
+
+  socket.onerror = (error) => {
+    console.log("there was an error");
+  };
+
+  socket.onmessage = (msg) => {
+    console.log(msg);
+    let j = JSON.parse(msg.data);
+    console.log(j);
+  };
+});
+
+const onlineUser = document.querySelector(".online");
+console.log(onlineUser);
+
+onlineUser.addEventListener("click", () => {
+  console.log("User name clicked...........");
+  let jsonData = {};
+  jsonData["action"] = "username";
+  jsonData["username"] = onlineUser.textContent;
+  socket.send(JSON.stringify(jsonData));
 });
