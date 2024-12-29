ba993031c38131d70d1886b250711484b3a19cc9
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-29 15:45:51 +0000

    implement search feature

--- static/scripts/ws.js
+++ static/scripts/ws.js
@@ -104,3 +104,33 @@ const whatOnYourMind = document.querySelector("#whatOnYourMind");
 whatOnYourMind.addEventListener("click", () => {
   postContainer.style.display = "block";
 });
+
+// ==============search feature====================
+const state = {
+  searchTerm: "",
+  filteredTerms: [],
+};
+
+const searchInput = document.querySelector("#search");
+searchInput.addEventListener("input", (e) => {
+  state.searchTerm = e.target.value.toLowerCase();
+  const posts = document.querySelectorAll(".post");
+
+  posts.forEach((post) => {
+    const title = post.querySelector("h3").textContent.toLowerCase();
+    const descriptions = post.querySelectorAll("p");
+    let descriptionText = "";
+    descriptions.forEach((desc) => {
+      descriptionText += desc.textContent.toLowerCase() + " ";
+    });
+
+    if (
+      title.includes(state.searchTerm) ||
+      descriptionText.includes(state.searchTerm)
+    ) {
+      post.style.display = "block";
+    } else {
+      post.style.display = "none";
+    }
+  });
+});
