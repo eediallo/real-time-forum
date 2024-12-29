5a8d0e78131a8e209330f5b5c8f0a050affb9b73
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-29 19:27:57 +0000

    moves somes queries to db

--- internal/handler/queries.go
+++ internal/handler/queries.go
@@ -14,44 +14,6 @@ const (
 			WHERE 
 				SessionID = ?`
 
-	postsWithDetailsQuery = `
-	SELECT
-		p.PostID,
-		p.Title, 
-		p.Content,
-		p.CommentCount,
-		STRFTIME('%d/%m/%Y, %H:%M', p.CreatedAt) AS CreatedAt,
-		p.Category,
-		u.Username
-	FROM 
-		Post AS p
-	INNER JOIN
-		User AS u
-	ON
-		p.UserID = u.UserID
-	ORDER BY
-		p.CreatedAt DESC
-	`
-	commentsFromDBQuery = `
-	SELECT
-		c.CommentID,
-		c.PostID,
-		c.Content,
-		c.LikeCount,
-		c.DislikeCount,
-		STRFTIME('%d/%m/%Y, %H:%M', c.CreatedAt) AS CreatedAt,
-		u.Username
-	FROM
-		Comments AS c
-	INNER JOIN
-		User AS u
-	ON
-		c.UserID = u.UserID
-	WHERE
-		c.PostID = ?
-	ORDER BY
-		(c.CreatedAt) ASC
-	`
 	userByEmailQuery = `
 	SELECT PasswordHash, UserID, NickName, Username
 	FROM User
