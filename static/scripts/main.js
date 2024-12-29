a6da8bceee45bfba7546aa4de6ac92e7086d22c9
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-29 21:02:34 +0000

    create message chat

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,4 +4,36 @@ function main() {
   eventHandlers();
 }
 
+//================================================================
+
+const onliUsers = document.querySelectorAll(".online");
+const dashboardContainer = document.querySelector(".dashboard-container");
+const middlePart = document.querySelector(".middle-part");
+let user = "";
+
+onliUsers.forEach((onlineUser) =>
+  onlineUser.addEventListener("click", (e) => {
+    let indexOfDash = onlineUser.textContent.indexOf("-");
+    user += onlineUser.textContent.slice(0, indexOfDash);
+    middlePart.innerHTML = ""; // clear dashboard container
+    createMessageChatBox(user);
+  })
+);
+
+function createMessageChatBox(username) {
+  const messageChatBox = document.createElement("section");
+  const user = document.createElement("h2");
+  user.textContent = username;
+  const chatDescription = document.createElement("p");
+  chatDescription.textContent = `This is the beginning of your direct message history with ${username}`;
+
+  const chatInput = document.createElement("input");
+  chatInput.type = "text";
+  chatInput.name = "chatInput";
+  chatInput.id = "chatInput";
+
+  messageChatBox.append(user, chatDescription, chatInput);
+  middlePart.append(messageChatBox);
+}
+
 window.onload = main;
