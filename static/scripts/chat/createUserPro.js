6a60500cb39e78129765babfbeff7d3a0891bf20
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-30 17:36:55 +0000

    adds link to full profile

--- static/scripts/chat/createUserPro.js
+++ static/scripts/chat/createUserPro.js
@@ -5,11 +5,14 @@ function createUserProfile(username) {
   const user = document.createElement("h2");
   user.textContent = username;
   const userProfile = `http://localhost:8080/users/${username}`;
-  const a = document.createElement("a");
-  a.textContent = `${username}#`;
-  a.setAttribute("href", userProfile);
+  const profileTag = document.createElement("p");
+  profileTag.textContent = `${username}#`;
 
-  profileSection.append(user, a);
+  const fullProfileLink = document.createElement("a");
+  fullProfileLink.setAttribute("href", userProfile);
+  fullProfileLink.textContent = "fullProfile#";
+
+  profileSection.append(user, profileTag, fullProfileLink);
   leftSide.append(profileSection);
 }
 
