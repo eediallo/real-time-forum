7f8db5612aa6d72119b280c6ab8e0bbe46201164
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-30 10:50:49 +0000

    get rid of white spaces of user receiving

--- static/scripts/initiateChat.js
+++ static/scripts/initiateChat.js
@@ -9,6 +9,7 @@ let chatBoxCreated = false;
 function initiateChatWithUser(onlineUser) {
   let indexOfDash = onlineUser.textContent.indexOf("-");
   userReceivingMsg = onlineUser.textContent.slice(0, indexOfDash);
+  userReceivingMsg = userReceivingMsg.trim();
   middlePart.innerHTML = ""; // clear middle section
   leftSide.innerHTML = "";
   if (!chatBoxCreated) {
