4eed64177828a45617365e7b74565a20f724ad0c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-30 21:11:03 +0000

    sent message to backend onsubmit

--- static/scripts/evenHandlers/initiateChatHandler.js
+++ static/scripts/evenHandlers/initiateChatHandler.js
@@ -1,10 +1,17 @@
 import { initiateChatWithUser } from "../initiateChat.js";
 import { onliUsers } from "../chat/config.js";
+import { sendChatContent } from "../chat/sendChatToBackend.js";
 
 function initiateChatHandler() {
   onliUsers.forEach((onlineUser) =>
     onlineUser.addEventListener("click", (e) => {
       initiateChatWithUser(onlineUser);
+
+      const chatForm = document.querySelector("form");
+      chatForm.onsubmit = (event) => {
+        event.preventDefault();
+        sendChatContent();
+      };
     })
   );
 }
