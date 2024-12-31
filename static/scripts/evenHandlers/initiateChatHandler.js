df9d2d239b911970bd9ac4927e8ab4ae338fec21
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-31 12:57:33 +0000

    display private message on the page

--- static/scripts/evenHandlers/initiateChatHandler.js
+++ static/scripts/evenHandlers/initiateChatHandler.js
@@ -7,6 +7,48 @@ function initiateChatHandler() {
     onlineUser.addEventListener("click", (e) => {
       initiateChatWithUser(onlineUser);
 
+      //display private messages ===========
+
+      const privateMessagesContainer =
+        document.querySelector(".privateMessages");
+
+      if (privateMessagesContainer) {
+        const currentUser = document.querySelector(".username").textContent;
+        console.log(currentUser);
+        const selectedUser =
+          document.querySelector(".receiver-username").textContent;
+        console.log(selectedUser);
+
+        fetch("/private_messages")
+          .then((response) => response.json())
+          .then((messages) => {
+            privateMessagesContainer.innerHTML = ""; // Clear previous messages
+            messages
+              .filter(
+                (message) =>
+                  (message.senderUsername === currentUser &&
+                    message.receiverUsername === selectedUser) ||
+                  (message.senderUsername === selectedUser &&
+                    message.receiverUsername === currentUser)
+              )
+              .forEach((message) => {
+                const messageElement = document.createElement("div");
+                messageElement.classList.add("message");
+                messageElement.innerHTML = `
+                  <p><strong>${message.senderUsername}</strong> to <strong>${message.receiverUsername}</strong></p>
+                  <p>${message.content}</p>
+                  <p><small>${message.createdAt}</small></p>
+                `;
+                privateMessagesContainer.appendChild(messageElement);
+              });
+          })
+          .catch((error) => {
+            console.error("Error fetching private messages:", error);
+          });
+      }
+
+      //========================================================
+
       const chatForm = document.querySelector("form");
       chatForm.onsubmit = (event) => {
         event.preventDefault();
