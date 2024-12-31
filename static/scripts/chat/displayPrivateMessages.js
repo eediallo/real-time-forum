11d007e87ae8e6010f7a9c55756352438a86613b
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2024-12-31 19:00:42 +0000

    change message display layout

--- static/scripts/chat/displayPrivateMessages.js
+++ static/scripts/chat/displayPrivateMessages.js
@@ -11,23 +11,44 @@ async function displayPrivateMessages() {
   }
 
   function renderPrivateMessages() {
-    messages
-      .filter(
-        (message) =>
-          (message.senderUsername === currentUser &&
-            message.receiverUsername === selectedUser) ||
-          (message.senderUsername === selectedUser &&
-            message.receiverUsername === currentUser)
-      )
-      .forEach((message) => {
+    const filteredMessages = messages.filter(
+      (message) =>
+        (message.senderUsername === currentUser &&
+          message.receiverUsername === selectedUser) ||
+        (message.senderUsername === selectedUser &&
+          message.receiverUsername === currentUser)
+    );
+
+    const groupedMessages = groupMessagesByDate(filteredMessages);
+
+    for (const [date, messages] of Object.entries(groupedMessages)) {
+      const dateElement = document.createElement("div");
+      dateElement.classList.add("date");
+      dateElement.innerHTML = `<p><strong>${date}</strong></p>`;
+      privateMessagesContainer.appendChild(dateElement);
+
+      messages.forEach((message) => {
         const messageElement = document.createElement("div");
         messageElement.classList.add("message");
         messageElement.innerHTML = `
-                                <p><strong>${message.senderUsername}</strong>: ${message.content}</p>
-                                <p><small>${message.createdAt}</small></p>
-                            `;
+                                                                    <p><strong>${message.senderUsername}</strong> <small>${message.createdAt}</small></p>
+                                                                    <p>${message.content}</p>
+                                                                    
+                                                            `;
         privateMessagesContainer.appendChild(messageElement);
       });
+    }
+  }
+
+  function groupMessagesByDate(messages) {
+    return messages.reduce((acc, message) => {
+      const date = new Date(message.createdAt).toLocaleDateString();
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(message);
+      return acc;
+    }, {});
   }
 }
 
